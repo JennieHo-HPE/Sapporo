b30daf776d8050bead48d16031245df281e86825
Daniel Schaefer <daniel.schaefer@hpe.com>
2020-10-08 14:44:57 +0800

    Add Rust and Golang

--- imports/library/lang_import.js
+++ imports/library/lang_import.js
@@ -4,5 +4,7 @@ import * as c_cpp      from 'brace/mode/c_cpp';
 import * as python     from 'brace/mode/python';
 import * as ruby       from 'brace/mode/ruby';
 import * as swift      from 'brace/mode/swift';
+import * as rust       from 'brace/mode/rust';
+import * as golang     from 'brace/mode/golang';
 
-export {javascript, c_cpp, python, java, ruby, swift};
+export {javascript, c_cpp, python, java, ruby, swift, rust, golang};
